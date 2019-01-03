885769f042377863ab68341b3f293098a463c3e7
outofgamut <jsalvie@gmail.com>
2019-01-02 18:15:32 -0600

    fix bug

--- gatsby-node.js
+++ gatsby-node.js
@@ -65,7 +65,7 @@ exports.createPages = ({ graphql, actions }) => {
         const next = i === posts.length - 1 ? null : posts[i + 1].node
         createPage({
           path: `${edge.node.slug}/`,
-          component: path.resolve(`./src/templates/ghost-post.js`),
+          component: path.resolve(`./src/templates/post.js`),
           context: {
             slug: edge.node.slug,
             prev,
